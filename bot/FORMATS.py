a63bccbb502ee2b8943498818663e5dda2136bc9
nxivm-bots <cyrilbinu2006@gmail.com>
2025-08-15 21:50:00 +0530

    Add bot/FORMATS.py

---
+++ bot/FORMATS.py
@@ -0,0 +1,172 @@
+on_pic = "https://telegra.ph/file/5593d624d11d92bceb48e.jpg"
+off_pic = "https://telegra.ph/file/0d9e590f62b63b51d4bf9.jpg"
+files_cmd_pic = "https://telegra.ph/file/d44f46054250a73053614.jpg"
+autodel_cmd_pic = "https://telegra.ph/file/a64533814021b40057ccd.jpg"
+
+PICS = [
+    "https://telegra.ph/file/f3d3aff9ec422158feb05-d2180e3665e0ac4d32.jpg",
+    "https://envs.sh/8gs.jpg"
+]
+
+#PICS = #"https://telegra.ph/file/f3d3aff9ec422158feb05-d2180e3665e0ac4d32.jpg #https://envs.sh/8gs.jpg".split() #Required
+
+START_PIC = "https://graph.org/file/4d765d3c53783f6ff670d-21029256a9aeab6a6a.jpg"
+FORCE_PIC = "https://graph.org/file/4d765d3c53783f6ff670d-21029256a9aeab6a6a.jpg"
+
+
+CUSTOM_CAPTION = "<b>» ʙʏ <a href=https://t.me/+RRPZEqvATG9iYTM1>ɴᴏᴠᴀ ғʟɪx</a></b>"
+
+#start message
+START_MSG = """<b>›› ʜᴇʏ!!, {mention} ~
+
+<blockquote>ʟᴏᴠᴇ ᴀɴɪᴍᴇs? ɪ ᴀᴍ ᴍᴀᴅᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ғɪɴᴅ ᴡʜᴀᴛ ʏᴏᴜ'ʀᴇ ʟᴏᴏᴋɪɴɢ ꜰᴏʀ.</blockquote></b>"""
+
+
+#Force sub message 
+FORCE_MSG =  """<b><blockquote>›› ʜᴇʏ {mention} ×</blockquote>\n  ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs</b>"""
+
+
+CMD_TXT = """<b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:
+
+<b>›› /batch :</b> ᴄʀᴇᴀᴛᴇ ɢʀᴏᴜᴘ ᴍᴇssᴀɢᴇs
+<b>›› /genlink :</b> ᴄʀᴇᴀᴛᴇ ʟɪɴᴋ ғᴏʀ ᴏɴᴇ ᴘᴏsᴛ
+<b>›› /broadcast :</b> ʙʀᴏᴀᴅᴄᴀsᴛ ᴍᴇssᴀɢᴇ
+<b>›› /broadcast silent:</b> sɪʟᴇɴᴛ ʙʀᴏᴀᴅᴄᴀsᴛ ᴍᴇssᴀɢᴇ
+<b>›› /autodel :</b> ᴠɪᴇᴡ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ sᴇᴛᴛɪɴɢs 
+<b>›› /restart :</b> ғᴏʀᴄᴇғᴜʟʟʏ ʀᴇsᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ (ᴏᴡɴᴇʀ)
+<b>›› /status :</b> ᴠɪᴇᴡ ʙᴏᴛ sᴛᴀᴛɪsᴛɪᴄs + ᴜᴘᴛɪᴍᴇ"""
+
+BAN_TXT = "<b><blockquote>Sᴏʀʀʏ, ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ</blockquote></b>"
+
+CHANNELS_TXT = """<b>›› ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/animes_cruise'>ᴀɴɪᴍᴇ ᴄʀᴜɪsᴇ</a>
+<blockquote expandable>›› ᴍᴏᴠɪᴇs: <a href='https://t.me/movieflixspot'>ᴍᴏᴠɪᴇғʟɪx sᴘᴏᴛ</a>
+›› ᴡᴇʙsᴇʀɪᴇs: <a href='https://t.me/webseries_flix'>ᴡᴇʙsᴇʀɪᴇs ғʟɪx</a>
+›› ᴀᴅᴜʟᴛ ᴄʜᴀɴɴᴇʟs: <a href='https://t.me/hanime_arena'>ᴄᴏʀɴʜᴜʙ</a>
+›› ᴍᴀɴʜᴡᴀ ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/pornhwa_flix'>ᴘᴏʀɴʜᴡᴀ</a>
+›› ᴄᴏᴍᴍᴜɴɪᴛʏ: <a href='https://t.me/otakuflix_network'>ᴏᴛᴀᴋᴜғʟɪx</a>
+›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @OnlyYuji | @rohit_1888</b></blockquote>"""
+
+HELP_TEXT = """<b>⁉️ Hᴇʟʟᴏ {mention} ~
+
+<blockquote expandable>➪ I ᴀᴍ ᴀ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇ sʜᴀʀɪɴɢ ʙᴏᴛ, ᴍᴇᴀɴᴛ ᴛᴏ ᴘʀᴏᴠɪᴅᴇ ғɪʟᴇs ᴀɴᴅ ɴᴇᴄᴇssᴀʀʏ sᴛᴜғғ ᴛʜʀᴏᴜɢʜ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ ғᴏʀ sᴘᴇᴄɪғɪᴄ ᴄʜᴀɴɴᴇʟs.
+
+➪ Iɴ ᴏʀᴅᴇʀ ᴛᴏ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇs ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ ᴀʟʟ ᴍᴇɴᴛɪᴏɴᴇᴅ ᴄʜᴀɴɴᴇʟ ᴛʜᴀᴛ ɪ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜ ᴛᴏ ᴊᴏɪɴ. Yᴏᴜ ᴄᴀɴ ɴᴏᴛ ᴀᴄᴄᴇss ᴏʀ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇs ᴜɴʟᴇss ʏᴏᴜ ᴊᴏɪɴᴇᴅ ᴀʟʟ ᴄʜᴀɴɴᴇʟs.
+
+➪ Sᴏ ᴊᴏɪɴ Mᴇɴᴛɪᴏɴᴇᴅ Cʜᴀɴɴᴇʟs ᴛᴏ ɢᴇᴛ Fɪʟᴇs ᴏʀ ɪɴɪᴛɪᴀᴛᴇ ᴍᴇssᴀɢᴇs...
+
+‣ /help -</b> Oᴘᴇɴ ᴛʜɪs ʜᴇʟᴘ ᴍᴇssᴀɢᴇ !</blockquote>
+<b><i>◈ Sᴛɪʟʟ ʜᴀᴠᴇ ᴅᴏᴜʙᴛs, ᴄᴏɴᴛᴀᴄᴛ ʙᴇʟᴏᴡ ᴘᴇʀsᴏɴs/ɢʀᴏᴜᴘ ᴀs ᴘᴇʀ ʏᴏᴜʀ ɴᴇᴇᴅ !</i></b>"""
+
+ABOUT_TXT = """<b>›› ᴍʏ ɴᴀᴍᴇ: {botname}
+<blockquote expandable>›› ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/codeflix_bots'>Cʟɪᴄᴋ ʜᴇʀᴇ</a>
+›› ᴏᴡɴᴇʀ: {ownername}
+›› ʟᴀɴɢᴜᴀɢᴇ: <a href='https://docs.python.org/3/'>Pʏᴛʜᴏɴ 3</a>
+›› ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ ᴠ2</a>
+›› ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>Mᴏɴɢᴏ ᴅʙ</a>
+›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @cosmic_freak | @rohit_1888</b></blockquote>"""
+
+SETTING_TXT = """<b>»  Cᴏɴғɪɢᴜʀᴀᴛɪᴏɴs</b>
+<blockquote expandable>» ᴛᴏᴛᴀʟ ғᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ:  <b>{total_fsub}</b>
+» ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴍᴏᴅᴇ:  <b>{autodel_mode}</b>
+» ᴘʀᴏᴛᴇᴄᴛ ᴄᴏɴᴛᴇɴᴛ:  <b>{protect_content}</b>
+» ʜɪᴅᴇ ᴄᴀᴘᴛɪᴏɴ:  <b>{hide_caption}</b>
+» ᴄʜᴀɴɴᴇʟ ʙᴜᴛᴛᴏɴ:  <b>{chnl_butn}</b>
+» ʀᴇǫᴜᴇsᴛ ғsᴜʙ ᴍᴏᴅᴇ: <b>{reqfsub}</b></blockquote>"""
+
+CHANNELS_TXT = """<b>›› ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/animes_cruise'>ᴀɴɪᴍᴇ ᴄʀᴜɪsᴇ</a>
+<blockquote expandable>›› ᴍᴏᴠɪᴇs: <a href='https://t.me/movieflixspot'>ᴍᴏᴠɪᴇғʟɪx sᴘᴏᴛ</a>
+›› ᴡᴇʙsᴇʀɪᴇs: <a href='https://t.me/webseries_flix'>ᴡᴇʙsᴇʀɪᴇs ғʟɪx</a>
+›› ᴀᴅᴜʟᴛ ᴄʜᴀɴɴᴇʟs: <a href='https://t.me/hanime_arena'>ᴄᴏʀɴʜᴜʙ</a>
+›› ᴍᴀɴʜᴡᴀ ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/pornhwa_flix'>ᴘᴏʀɴʜᴡᴀ</a>
+›› ᴄᴏᴍᴍᴜɴɪᴛʏ: <a href='https://t.me/otakuflix_network'>ᴏᴛᴀᴋᴜғʟɪx</a>
+›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @OnlyYuji | @rohit_1888</b></blockquote>"""
+
+on_txt, off_txt = "Eɴᴀʙʟᴇᴅ ✅", "Dɪsᴀʙʟᴇᴅ ❌"
+
+FILES_CMD_TXT ="""<b>ғɪʟᴇs ʀᴇʟᴀᴛᴇᴅ sᴇᴛᴛɪɴɢs:
+
+<blockquote expandable>›› ᴘʀᴏᴛᴇᴄᴛ ᴄᴏɴᴛᴇɴᴛ: {protect_content}
+›› ʜɪᴅᴇ ᴄᴀᴘᴛɪᴏɴ: {hide_caption}
+›› ᴄʜᴀɴɴᴇʟ ʙᴜᴛᴛᴏɴ: {channel_button}</b>
+
+›› ʙᴜᴛᴛᴏɴ Nᴀᴍᴇ: {name}
+›› ʙᴜᴛᴛᴏɴ Lɪɴᴋ: {link}</blockquote>
+
+<b>ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ᴛᴏ ᴄʜᴀɴɢᴇ sᴇᴛᴛɪɴɢs</b>"""
+
+AUTODEL_CMD_TXT = """<b>» ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ sᴇᴛᴛɪɴɢs
+
+<blockquote>›› ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴍᴏᴅᴇ: {autodel_mode}</blockquote>
+<blockquote>›› ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇʀ: {timer}</blockquote>
+
+ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ᴛᴏ ᴄʜᴀɴɢᴇ sᴇᴛᴛɪɴɢs</b>"""
+
+FSUB_CMD_TXT = """<b>» ғᴏʀᴄᴇ sᴜʙ ᴄᴏᴍᴍᴀɴᴅs :</b>
+
+<b>›› /channels</b> : ᴄʜᴇᴄᴋ ᴄᴜʀʀᴇɴᴛ ғᴏʀᴄᴇ-sᴜʙ ᴄʜᴀɴɴᴇʟs (ᴀᴅᴍɪɴs)
+<b>›› /addfsub</b> : ᴀᴅᴅ ᴏɴᴇ ᴏʀ ᴍᴜʟᴛɪᴘʟᴇ ғᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟs (ᴏᴡɴᴇʀ)
+<b>›› /delfsub</b> : ᴅᴇʟᴇᴛᴇ ᴏɴᴇ ᴏʀ ᴍᴜʟᴛɪᴘʟᴇ ғᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟs (ᴏᴡɴᴇʀ)
+<b>›› /req</b> : ʀᴇǫᴜᴇsᴛ ғsᴜʙ ᴍᴏᴅᴇ (ᴏᴡɴᴇʀ)"""
+
+
+USER_CMD_TXT = """<b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs: </b>
+    
+<b>/admins</b> : ᴠɪᴇᴡ ᴛʜᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ᴀᴅᴍɪɴ ʟɪsᴛ (ᴏᴡɴᴇʀ)
+<b>/addadmin</b> : ᴀᴅᴅ ᴏɴᴇ ᴏʀ ᴍᴜʟᴛɪᴘʟᴇ ᴜsᴇʀ ɪᴅs ᴀs ᴀᴅᴍɪɴ (ᴏᴡɴᴇʀ)
+<b>/deladmin</b> : ᴅᴇʟᴇᴛᴇ ᴏɴᴇ ᴏʀ ᴍᴜʟᴛɪᴘʟᴇ ᴜsᴇʀ ɪᴅs ғʀᴏᴍ ᴀᴅᴍɪɴs (ᴏᴡɴᴇʀ)
+
+<b>» ʙᴀɴ-ᴜɴʙᴀɴ ᴜsᴇʀ</b>
+<b>/banlist</b> : ᴠɪᴇᴡ ᴛʜᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ʙᴀɴɴᴇᴅ ᴜsᴇʀ ʟɪsᴛ (ᴏᴡɴᴇʀ)
+<b>/ban</b> : ᴀᴅᴅ ᴏɴᴇ ᴏʀ ᴍᴜʟᴛɪᴘʟᴇ ᴜsᴇʀ ɪᴅs ɪɴ ʙᴀɴɴᴇᴅ ʟɪsᴛ (ᴏᴡɴᴇʀ)
+<b>/unban</b> : ᴅᴇʟᴇᴛᴇ ᴏɴᴇ ᴏʀ ᴍᴜʟᴛɪᴘʟᴇ ᴜsᴇʀ ɪᴅs ғʀᴏᴍ ʙᴀɴɴᴇᴅ ʟɪsᴛ (ᴏᴡɴᴇʀ)"""
+
+
+
+RFSUB_CMD_TXT = """<b>›› ʀᴇǫᴜᴇsᴛ ғsᴜʙ sᴇᴛᴛɪɴɢs:
+
+<blockquote><b>» ʀᴇǫᴜᴇsᴛ ғsᴜʙ ᴍᴏᴅᴇ: {req_mode}</b></blockquote>
+
+ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ᴛᴏ ᴄʜᴀɴɢᴇ sᴇᴛᴛɪɴɢs</b>"""
+
+
+RFSUB_MS_TXT = """<b>›› ʀᴇǫᴜᴇsᴛ ғsᴜʙ ʟɪsᴛ:
+
+<blockquote expandable>{reqfsub_list}</blockquote>
+ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ᴛᴏ ᴄʜᴀɴɢᴇ sᴇᴛᴛɪɴɢs</b>"""
+
+CLEAR_USERS_TXT = """<blockquote expandable><b>What is the use of Clear Users !?</b>
+
+➪ Cʟᴇᴀʀ Usᴇʀs ɪs ᴜsᴇᴅ ᴛᴏ ᴄʟᴇᴀʀ ᴛʜᴇ ᴀʟʟ ᴜsᴇʀ ᴅᴀᴛᴀ ᴏғ ᴀ sᴘᴇᴄɪғɪᴇᴅ Rᴇǫᴜᴇsᴛ ғᴏʀᴄᴇsᴜʙ cʜᴀɴɴᴇʟ ɪᴅ. 
+
+➪ Hᴇʀᴇ oɴʟʏ usᴇʀ ᴅᴀᴛᴀ ɪs ᴅᴇʟᴇᴛᴇᴅ ғʀᴏᴍ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ.</blockquote>
+
+<b><i>Cʜᴏᴏsᴇ ᴛʜᴇ Cʜᴀɴɴᴇʟ ɪᴅ ғᴏʀ ᴅᴇʟᴇᴛɪɴɢ ᴜsᴇʀ ᴅᴀᴛᴀ:</i></b>"""
+
+
+CLEAR_CHNLS_TXT = """<blockquote expandable><b>What is the use of Clear Channels !?</b>
+
+➪ Cʟᴇᴀʀ Cʜᴀɴɴᴇʟs ᴜsᴇᴅ ᴛᴏ Dᴇʟᴇᴛᴇ ᴀʟʟ ᴜsᴇʀ ᴅᴀᴛᴀ ᴀʟᴏɴɢ ᴡɪᴛʜ Rᴇǫᴜᴇsᴛ ғᴏʀᴄᴇsᴜʙ ᴄʜᴀɴɴᴇʟ ɪᴅ ᴀɴᴅ ʟɪɴᴋ ғʀᴏᴍ ᴅᴀᴛᴀʙᴀsᴇ. 
+
+➪ Hᴇʀᴇ ᴀʟʟ ᴅᴀᴛᴀ ʀᴇᴀʟᴛᴇᴅ ᴛᴏ ʀᴇǫᴜᴇsᴛ ғᴏʀᴄᴇsᴜʙ ᴄʜᴀɴɴᴇʟ ɪᴅ ᴅᴇʟᴇᴛᴇᴅ Pᴇʀᴍᴀɴᴇɴᴛʟʏ...
+
+<b>⚠️ WARNING:</b> Cʟᴇᴀʀ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ᴅᴀᴛᴀ ᴏɴʟʏ ᴡʜᴇɴ ɪᴛ ɪs ᴄᴏɴғɪʀᴍᴇᴅ ᴛʜᴀᴛ ᴛʜᴇ ᴅᴀᴛᴀ ᴡɪʟʟ ɴᴏ ʟᴏɴɢᴇʀ ʙᴇ ʀᴇǫᴜɪʀᴇᴅ ғᴏʀ ғᴜᴛᴜʀᴇ ᴏᴘᴇʀᴀᴛɪᴏɴs.</blockquote>
+
+<b><i>Cʜᴏᴏsᴇ ᴛʜᴇ Cʜᴀɴɴᴇʟ ɪᴅ ғᴏʀ ᴅᴇʟᴇᴛɪɴɢ:</i></b>"""
+
+
+CLEAR_LINKS_TXT = """<blockquote expandable><b>What is the use of Clear Links !?</b>
+
+➪ Cʟᴇᴀʀ Sᴛᴏʀᴇᴅ Rᴇǫᴜᴇsᴛ Lɪɴᴋs ᴜsᴇᴅ ᴛᴏ Dᴇʟᴇᴛᴇ Lɪɴᴋs ᴏғ ᴀ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ ᴀs ᴡᴇʟʟ ᴀs ʀᴇᴠᴏᴋᴇ ᴛʜᴇ ʟɪɴᴋ ғʀᴏᴍ ᴛʜᴀᴛ Cʜᴀɴɴᴇʟ. 
+
+➪ Eᴠᴇɴ ɪғ ᴄʟᴇᴀʀɪɴɢ ᴄʜᴀɴɴᴇʟ ᴅᴀᴛᴀ ᴛʜᴇ Rᴇǫᴜᴇsᴛ Lɪɴᴋ sᴛᴏʀᴇᴅ ᴏɴ ᴅᴀᴛᴀʙᴀsᴇ ғᴏʀ ғᴜᴛᴜʀᴇ ᴜsɪɴɢ ᴏғ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ, 
+
+➪ Bʏ ᴅᴇʟᴇᴛɪɴɢ ʀᴇǫᴜᴇsᴛ ʟɪɴᴋ ᴏғ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴛʜᴇ ʟɪɴᴋ ᴡɪʟʟ ʙᴇ ʀᴇᴠᴏᴋᴇᴅ ғʀᴏᴍ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ɴᴏᴛ ᴜsᴀʙʟᴇ ᴀɴʏ ᴍᴏʀᴇ, 
+
+➪ Sᴏ ᴛʜᴇ ʙᴏᴛ ᴡɪʟʟ ʜᴀᴠᴇ ᴛᴏ ᴄʀᴇᴀᴛᴇ ᴀɢᴀɪɴ ʀᴇǫᴜᴇsᴛ ʟɪɴᴋ ᴏғ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ ɪɴ ғᴜᴛᴜʀᴇ ɪғ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ ᴀɢᴀɪɴ ᴀᴅᴅᴇᴅ ᴀs ʀᴇǫᴜᴇsᴛ ғᴏʀᴄᴇsᴜʙ ᴄʜᴀɴɴᴇʟ.
+
+<b>⚠️ NOTE:</b> 
+‣ Tᴏ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ ᴛʜᴇ ʙᴏᴛ sʜᴏᴜʟᴅ ʜᴀᴠᴇ ᴀᴅᴍɪɴ ᴀʟᴏɴɢ ᴡɪᴛʜ ᴘʀᴏᴘᴇʀ ᴘᴇʀᴍɪssɪᴏɴ ᴏɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ. 
+
+‣ ɪғ ᴛʜᴇ ʙᴏᴛ ɴᴏᴛ ɪɴ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ ᴏʀ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴ ᴛʜᴇɴ ᴛʜɪs ᴏᴘᴇʀᴀᴛɪᴏɴ ᴄᴀɴ'ᴛ ʙᴇ ᴘᴇʀғᴏʀᴍᴇᴅ.</blockquote>
+
+<b><i>Cʜᴏᴏsᴇ ᴛʜᴇ Cʜᴀɴɴᴇʟ ɪᴅ ғᴏʀ ᴅᴇʟᴇᴛɪɴɢ Rᴇǫᴜᴇsᴛ Lɪɴᴋ:</i></b>"""
