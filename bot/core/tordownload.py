9bcca2a09e8a79e90e01ce5d78d5fafa27001765
nxivm-bots <cyrilbinu2006@gmail.com>
2025-08-15 21:50:19 +0530

    Add bot/core/tordownload.py

---
+++ bot/core/tordownload.py
@@ -0,0 +1,45 @@
+import os # You might already have this for other path operations
+from aiofiles import os as aio_os # This is crucial for async file operations
+from os import path as ospath
+from aiofiles import open as aiopen
+from aiofiles.os import path as aiopath, remove as aioremove, mkdir
+
+from aiohttp import ClientSession
+from torrentp import TorrentDownloader
+from bot import LOGS
+from bot.core.func_utils import handle_logs
+
+class TorDownloader:
+    def __init__(self, path="."):
+        self.__downdir = path
+        self.__torpath = "torrents/"
+    
+    @handle_logs
+    async def download(self, torrent, name=None):
+        if torrent.startswith("magnet:"):
+            torp = TorrentDownloader(torrent, self.__downdir)
+            await torp.start_download()
+            return ospath.join(self.__downdir, name)
+        elif torfile := await self.get_torfile(torrent):
+            torp = TorrentDownloader(torfile, self.__downdir)
+            await torp.start_download()
+            await aioremove(torfile)
+            return ospath.join(self.__downdir, torp._torrent_info._info.name())
+
+    @handle_logs
+    async def get_torfile(self, url):
+        if not await aiopath.isdir(self.__torpath):
+            await aio_os.makedirs(self.__torpath, exist_ok=True)
+        
+        tor_name = url.split('/')[-1]
+        des_dir = ospath.join(self.__torpath, tor_name)
+        
+        async with ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    async with aiopen(des_dir, 'wb') as file:
+                        async for chunk in response.content.iter_any():
+                            await file.write(chunk)
+                    return des_dir
+        return None
+        
